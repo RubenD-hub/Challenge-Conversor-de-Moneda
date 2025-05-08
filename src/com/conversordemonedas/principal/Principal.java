75bfa5d65930e800b618333e98a3477085da07be
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-05-08 16:05:59 -0600

    Creando archivo principal

---
+++ src/com/conversordemonedas/principal/Principal.java
@@ -0,0 +1,9 @@
+package com.conversordemonedas.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
