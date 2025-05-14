b02fa2d51337e9cd0b2c40041cead59282eda3ec
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-05-13 21:48:28 -0600

    Clase, que hace la consulta a la API y retorna la estructura Moneda de la peticion

---
+++ src/com/conversordemonedas/modulos/ConsultaMoneda.java
@@ -0,0 +1,31 @@
+package com.conversordemonedas.modulos;
+
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+    public Moneda buscarDivisa(TipoDeConversion conversion) {
+        String monedaInicial = conversion.monedaInicial();
+        String monedaFinal = conversion.monedaFinal();
+
+
+        URI dirURL = URI.create("https://v6.exchangerate-api.com/v6/88666fd2c4743fadc8c2df86/latest/" + monedaInicial);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(dirURL)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No esncontre esa Moneda.");
+        }
+    }
+}
