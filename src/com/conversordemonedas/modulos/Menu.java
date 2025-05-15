1d934ff9c03a5dd6aaf4a5a1f1b47b6fe56b3732
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-05-14 23:45:44 -0600

    Metodo para saber si se quiere hacer otra conversion

--- src/com/conversordemonedas/modulos/Menu.java
+++ src/com/conversordemonedas/modulos/Menu.java
@@ -10,7 +10,6 @@ public class Menu {
     }
 
     public void mostrarMenu(List<TipoDeConversion> lista) {
-
         System.out.println("\t==========  MENÚ  ==========");
         for (int i = 0; i < lista.size(); i++) {
             System.out.printf("\t%d. %s\n", i + 1, lista.get(i).conversion());
@@ -19,21 +18,35 @@ public class Menu {
     }
 
     public int mostrarYSeleccionar(List<TipoDeConversion> lista) {
-
         mostrarMenu(lista);
-
-        int opcion;
         while (true) {
-
             System.out.print("\n\t-> ¿Cuál es la opción que desea hacer?: ");
-
             try {
-                opcion = scan.nextInt();
-                if (opcion < 1 || opcion > lista.size() + 1) {
+                int opc = scan.nextInt();
+                if (opc < 1 || opc > lista.size() + 1) {
                     System.out.println("\t¡¡Opción incorrecta!!\n\tVuelva a intentarlo.");
                     continue;
                 }
-                return opcion; // Opción válida
+                // Devuelve la respuesta correspondiente
+                return opc;
+            } catch (InputMismatchException e) {
+                System.out.println("\t¡¡Error: debe ingresar un número!!\n\tVuelva a intentarlo.");
+                scan.nextLine(); // Limpiar el buffer
+            }
+        }
+    }
+
+    public String OtraConversion() {
+        while (true) {
+            System.out.print("\n\t-> ¿Desea hacer otra conversión?\n\t\t1) Sí\n\t\t2) No\n\t\t-> ");
+            try {
+                int opc = scan.nextInt();
+                if (opc < 1 || opc > 2) {
+                    System.out.println("\t¡¡Opción incorrecta!!\n\tVuelva a intentarlo.");
+                    continue;
+                }
+                // Devuelve la respuesta correspondiente
+                return opc == 1 ? "Si" : "No";
             } catch (InputMismatchException e) {
                 System.out.println("\t¡¡Error: debe ingresar un número!!\n\tVuelva a intentarlo.");
                 scan.nextLine(); // Limpiar el buffer
