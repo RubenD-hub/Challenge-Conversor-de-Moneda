8ae1ec93433727b0fecae3128c856342dd897465
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-07-28 11:22:52 -0600

    feat(GenerarHistorial): Creacion de metodos para mostrar y guardar el historial guardado en los registros

---
+++ src/com/conversordemonedas/modulos/GenerarHistorial.java
@@ -0,0 +1,58 @@
+package com.conversordemonedas.modulos;
+
+import com.google.gson.*;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.conversordemonedas.estilos.EstilosAnsi.*;
+
+
+public class GenerarHistorial {
+    private final List<Registro> registros = new ArrayList<>();
+    private static final String archivoJson = "Historial.json";
+
+    public void agregarRegistro(String monedaInicial, float monto, String monedaFinal, float conversion) {
+        Registro registro = new Registro(monedaInicial, monto, monedaFinal, conversion);
+        registros.add(registro);
+        guardarHistorial();
+    }
+
+    public void mostrarRegistro() {
+        if (registros.isEmpty()) {
+            System.out.println(VERDE + "\n\n\t\t═══════════════════════════");
+            System.out.println(MAGENTA + "\t\t⚠️ Historial no previo" + RESET);
+            System.out.println(VERDE + "\t\t═══════════════════════════\n\n\n\n");
+        } else {
+            System.out.println(MAGENTA + "\n♦♦♦♦♦♦♦♦♦♦♦  Historial de Conversiones  ♦♦♦♦♦♦♦♦♦♦♦" + RESET);
+            for (Registro registro : registros) {
+                System.out.println(registro);
+            }
+        }
+    }
+
+    private void guardarHistorial() {
+        try (FileWriter writer = new FileWriter(archivoJson)) {
+//            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .registerTypeAdapter(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+                        @Override
+                        public JsonElement serialize(LocalDateTime src, java.lang.reflect.Type typeOfSrc, JsonSerializationContext context) {
+                            return new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                        }
+                    })
+                    .create();
+            gson.toJson(registros, writer);
+        } catch (IOException e) {
+            System.out.println(ROJO + "❌ Error al guardar el archivo: " + e.getMessage() + RESET);
+        }
+    }
+}
+
